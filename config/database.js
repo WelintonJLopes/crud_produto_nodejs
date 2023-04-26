a8f55c6e7eea90e410f8ca94ecbec3b945893f9c
WelintonJLopes <welintonj.lopes@hotmail.com>
2023-04-25 21:29:28 -0300

    Adicionado comunicação com Firebasee ajuste de models

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
+const admin = require('firebase-admin');
 
+// Configuração do banco PostgreSQL
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
@@ -8,6 +10,15 @@ const pool = new Pool({
     port: 5432,
 });
 
+// Configuração do banco Firebase
+const serviceAccount = require('../../dbproductsf-firebase-adminsdk-mi1ne-ee1428b1ad.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://dbproductsf-default-rtdb.firebaseio.com/"
+});
+
 module.exports = {
     query: (text, params) => pool.query(text, params),
+    firebase: admin
 };
--- models/productModel.js
+++ models/productModel.js
@@ -3,12 +3,31 @@ const db = require('../config/database');
 class Product {
     static async findAll() {
         const { rows } = await db.query('SELECT * FROM products');
-        return rows;
+
+        if (rows.length > 0) {
+            return rows;
+        } else {
+            const snapshot = await db.firebase.database().ref('products').once('value');
+            const products = snapshot.val();
+            return Object.keys(products).map(key => ({ id: key, ...products[key] }));
+        }
     }
 
     static async findById(id) {
         const { rows } = await db.query('SELECT * FROM products WHERE id = $1', [id]);
-        return rows[0];
+
+        if (rows.length > 0) {
+            return rows[0];
+        } else {
+            const snapshot = await db.firebase.database().ref('/products').orderByChild('id').equalTo(parseInt(id)).once('value');
+            const product = snapshot.val();
+
+            if (product) {
+                return product;
+            } else {
+                throw new Error(`Product with id ${id} not found`);
+            }
+        }
     }
 
     static async create(name, description) {
@@ -16,6 +35,14 @@ class Product {
             name,
             description,
         ]);
+
+        const firebaseRef = db.firebase.database().ref('products');
+        await firebaseRef.push({
+            id: rows[0].id,
+            name: rows[0].name,
+            description: rows[0].description
+        });
+
         return rows[0];
     }
 
@@ -25,11 +52,28 @@ class Product {
             description,
             id,
         ]);
+
+        const snapshot = await db.firebase.database().ref('/products').orderByChild('id').equalTo(parseInt(id)).once('value');
+        const product = snapshot.val();
+        const key = Object.keys(product)[0]
+        const updates = {};
+        updates[`/products/${key}/name`] = name;
+        updates[`/products/${key}/description`] = description;
+
+        await db.firebase.database().ref().update(updates);
+
         return rows[0];
     }
 
     static async delete(id) {
         const { rowCount } = await db.query('DELETE FROM products WHERE id = $1', [id]);
+
+        const snapshot = await db.firebase.database().ref('/products').orderByChild('id').equalTo(parseInt(id)).once('value');
+        const product = snapshot.val();
+        const key = Object.keys(product)[0]
+        const productRef = db.firebase.database().ref('products').child(key);
+        await productRef.remove();
+
         return rowCount;
     }
 }
